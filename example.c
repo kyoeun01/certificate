b4f510e109b41efe7a3eb9ac858e50e6aa84704d
kyoeun01 <101385806+kyoeun01@users.noreply.github.com>
2022-05-05 23:53:19 +0900

    Update example.c

--- example.c
+++ example.c
@@ -1,1 +1,21 @@
-#include
+#include <stdio.h>
+
+int main()
+{
+  int tarr[3][3] =
+  {
+    {1,2,3},
+    {4,5,6}
+  };
+
+  for(int i=0; i<2; i++)
+  {
+    for {int j=0; j<3; j++)
+    {
+      printf("%d ", tarr[i][j]);
+    }
+    printf("\n")
+  }
+    
+  return 0;
+  }
