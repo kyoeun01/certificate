feb264ce9b6defed937e199c45a9662c8c8315a8
kyoeun01 <101385806+kyoeun01@users.noreply.github.com>
2022-05-06 23:26:12 +0900

    Create example2.c

---
+++ example2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h
+#include <math.h>
+#include <stlib.h>
+
+main()
+{
+  printf("%f\n", pow(2,3));
+  
+  printf"%f\n", sqrt(25));
+  
+  printf("%d\n", abs9(-20));
+  
+  char a[] = "ad";
+  char b[] = "ac";
+  printf("%d\n", strcmp(a,b));
+  
+  strcpy(a,b);
+  printf("%d\n", strcmp(a,b));
+  
+  char c[] = "123";
+  char d[] = "456";
+  printf("%d\n", atoi(c) + atoi(d));
+  
+  char e[] = "12.3";
+  char f[] = "45.6";
+  printf("%d\n", atoi(e) + atoi(f));
+}
