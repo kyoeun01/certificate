d16269e5802b3541c774eb4b9b6b92c8f821dc8e
kyoeun01 <101385806+kyoeun01@users.noreply.github.com>
2022-05-14 21:04:59 +0900

    Update example6.c

--- example6.c
+++ example6.c
@@ -4,28 +4,31 @@ main()
 {
   int a, b, big, small, mok, nmg, gcm, lcm;
   scanf("%d %d, &a, &b);
-        if(a >= b)
-        {
-          big = a;
-          small =b;
-        }
-        else
-        {
-          big = b;
-          small = a;
-        }
-        while (1)
-        {
-          mok = big / small;
-          nmg = big - mok * small;
-          if (nmg == 0)
-          {
-            gcm = small;
-            lcm = a * b / gcm;
-            printf("%d %d, gcm, lcm);
-            break;
-          }
-          big = small;
-          small = nmg;
-        }
+        
+  if(a >= b)
+  {
+    big = a;
+    small = b; // 수행 후 while문
+  }
+  else
+  {
+    big = b;
+    small = a; // 수행 후 while문
+  }
+        
+  while (1)
+  {
+     mok = big / small;
+     nmg = big - mok * small;
+    
+     if (nmg == 0)
+     {
+       gcm = small;
+       lcm = a * b / gcm;
+       printf("%d %d, gcm, lcm);
+       break;
+     }              
+     big = small;
+     small = nmg;
+  }
 }                   
