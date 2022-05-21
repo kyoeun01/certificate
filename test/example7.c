a820f4cb6eeec6ebccccd73d759d7ed598ab75ff
kyoeun01 <101385806+kyoeun01@users.noreply.github.com>
2022-05-21 22:08:37 +0900

    Create example7.c

---
+++ test/example7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+main()
+{
+  int b, mok, nmg. i;
+  int a[100];
+  
+  scanf("%d", &b);
+  int c = 0, d = -1;
+  
+  while (1)
+  {
+    c++;
+    if (c <= b)
+    {
+      mok = b / c;
+      nmg = b - mok * c;
+      if (nmg == 0)
+      {
+        d++;
+        a[d] = c;
+      }
+    }
+    else
+    {
+      printf("%d ", b);
+      for (i = 0; i <= d; i++)
+        
+        printf("%d ", a[i]);
+      break;
+    }
+  }
+}
